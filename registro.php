d30c9f3ff1e78ba458264b84aa5427503981881a
Cristian Schweizer <schweizercristian@gmail.com>
2018-03-22 19:36:16 -0300

    Subo la página de registro

--- registro.php
+++ registro.php
@@ -3,10 +3,62 @@
   <head>
     <meta charset="utf-8">
     <title>Registro</title>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./css/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
     <div class="container">
-      
+      <header class="cabecera">
+        <h2 class="tituloPrincipal">eCommerce Primero</h2>
+        <nav >
+          <ul class="navegador">
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">CONTACTO</a></li>
+              <li><a href="#">LOGIN</a></li>
+              <li><a href="#">NOSOTROS</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section class="seccionRegistro">
+        <h3>Por favor, complete con sus datos para el registro</h3>
+        <form class="" action="index.html" method="post">
+          <div class="form-group">
+            <label for="" class="labelForm">Nombre</label>
+            <input type="text" name="" value="" class="input-cris">
+          </div>
+          <div class="form-group">
+            <label for="" class="labelForm">Apellido</label>
+            <input type="text" name="" value="" class="input-cris">
+          </div>
+          <div class="form-group">
+            <label for="" class="labelForm">Email</label>
+            <input type="email" name="" value="" class="input-cris">
+          </div>
+          <div class="form-group">
+            <label for="" class="labelForm">Contraseña</label>
+            <input type="password" name="" value="" class="input-cris">
+          </div>
+          <br>
+          <div class="labelForm">
+            <button type="submit" name="button" class="btn btn-primary">Enviar</button>
+          </div>
+          <br>
+        </form>
+      </section>
+      <footer class="piePag">
+        <ul class="listaRedes">
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+          <li>Contacto</li>
+        </ul>
+      </footer>
+    </div>
+    <div>
+      <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </div>
   </body>
 </html>
